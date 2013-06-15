2260e72bb841561cc5755f7112c6f3c8b0eb9a98
fluidbyte <kent@fluidbyte.net>
2013-06-15 16:18:36 +0000

    Added key check authentication

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+// Define Configuration
+
+var config = {
+    // Authentication keys
+    keys: [
+        '12345',
+        '67890'
+    ],
+    // Port designation
+    port: 8080
+};
+
 
 // Set up Restify
 var restify = require('restify');
@@ -14,9 +26,26 @@ server.use(restify.bodyParser());
 var reqRegEx = /^\/([a-zA-Z0-9_\.~-]+)\/([a-zA-Z0-9_\.~-]+)\/(.*)/;
 
 /**
+ * Check Key
+ */
+ 
+var checkKey = function (config, req, res) {
+    // Loop through keys in config
+    for (var i = 0, z = config.keys.length; i < z; i++) {
+        if (config.keys[i] === req.params[0]) {
+            return true;
+        }
+    }
+    // Failed key, 401 - Unauthorized
+    res.send(401);
+    return false;
+};
+
+/**
  * GET
  */
 server.get(reqRegEx, function (req, res, next) {
+    checkKey(config, req, res);
     res.send(req.params);
     return next();
 });
@@ -38,8 +67,16 @@ server.post(reqRegEx, function (req, res, next) {
 });
 
 /**
+ * DELETE
+ */
+server.del(reqRegEx, function (req, res, next) {
+    res.send(req.params);
+    return next();
+});
+
+/**
  * START SERVER
  */
-server.listen(3000, function () {
+server.listen(config.port, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
